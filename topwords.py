bbf5d9eb7e0a80fbb9cc9de3d92da8ac0fa6abf9
Anton Panchishin <anton.panchishin@gmail.com>
2017-07-03 17:39:19 +0000

    remove poor words

--- topwords.py
+++ topwords.py
@@ -1,7 +1,7 @@
 import numpy as np
 import json
 
-words = [ 'lily','rose','hydrangea','kalanchoe','hibiscus','begonia','hosta','iris','coleus','maple','geranium','peony','azalea','aloe','orchid','ivy','sedum','petunia','dracaena','dahlia','dianthus','red','virginia','bromeliad','japonica','magnolia','pothos','oak','schefflera','clematis','creeper','rhododendron','cyclamen','fern','jade','lantana','dieffenbachia','daisy','tulip','cactus','croton','columbine','celosia','ficus','hyacinth','yucca','salvia','canna','euphorbia','japanese','poppy','violet','palm','peace','poison','strawberry','day','camellia','morning','oxalis','impatiens','dogwood','phlox','glory','spathiphyllum','allium','vera','spider','vinca','african','wild','sweet','christmas','amaryllis','black' ]
+words = [ 'lily','rose','hydrangea','kalanchoe','hibiscus','begonia','hosta','iris','coleus','maple','geranium','peony','azalea','aloe','orchid','ivy','sedum','petunia','dracaena','dahlia','dianthus','virginia','bromeliad','japonica','magnolia','pothos','oak','schefflera','clematis','creeper','rhododendron','cyclamen','fern','jade','lantana','dieffenbachia','daisy','tulip','cactus','croton','columbine','celosia','ficus','hyacinth','yucca','salvia','canna','euphorbia','japanese','poppy','violet','palm','strawberry','camellia','oxalis','impatiens','dogwood','phlox','glory','spathiphyllum','allium','vera','vinca','amaryllis' ]
 
 confirmed = np.array(json.load( open("meta-data/confirmed_ids_with_top_words.json","r")))
 all_data = json.load( open("meta-data/all_data.json","r") )
