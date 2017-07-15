c43829d2dd03d091305a43d51fff53ad6147721f
panchishin <anton@panchishin.com>
2017-07-15 11:38:22 -0400

    reduce stages without additional error

--- gardengraph.py
+++ gardengraph.py
@@ -15,33 +15,29 @@ class cnn :
     self.training       = tf.placeholder( tf.bool, name="is_training_cycle" )
     
     stages = []
+
     stages.append( layer.batch_normalization( self.x , training=self.training ) )
     stages.append( layer.conv_relu( stages[-1] , 3 , 16 , width=7, padding='SAME', name="3x16" ) )
-    stages.append( layer.resnet_block( stages[-1] , 16, 3 , training=self.training, momentum=0.99, name="16" ) )
 
     stages.append( layer.resnet_downscale( stages[-1] ) )
     stages.append( layer.resnet_block( stages[-1] , 32, 3 , training=self.training, momentum=0.99, name="32" ) )
-    # 2
+
     stages.append( layer.resnet_downscale( stages[-1] ) )
     stages.append( layer.resnet_block( stages[-1] , 64, 3 , training=self.training, momentum=0.99, name="64" ) )
-    # 4
+
     stages.append( layer.resnet_downscale( stages[-1] ) )
     stages.append( layer.resnet_narrow( stages[-1] , 128, 3 , training=self.training, narrowing=4, momentum=0.99, name="128_1" ) )
-    stages.append( layer.resnet_narrow( stages[-1] , 128, 3 , training=self.training, narrowing=4, momentum=0.99, name="128_2" ) )
-    # 8
+
     stages.append( layer.resnet_downscale( stages[-1] ) )
     stages.append( layer.resnet_narrow( stages[-1] , 256, 3 , training=self.training, narrowing=8, momentum=0.99, name="256_1" ) )
-    stages.append( layer.resnet_narrow( stages[-1] , 256, 3 , training=self.training, narrowing=8, momentum=0.99, name="256_2" ) )
-    # 16
+
     stages.append( layer.resnet_downscale( stages[-1] ) )
     stages.append( layer.resnet_narrow( stages[-1] , 512, 3 , training=self.training, narrowing=16, momentum=0.99, name="512_1" ) )
-    stages.append( layer.resnet_narrow( stages[-1] , 512, 3 , training=self.training, narrowing=16, momentum=0.99, name="512_2" ) )
-    # 32
-    stages.append( layer.avg_pool( stages[-1] , stride=4 ) )
-    # 4*32=128
-    stages.append( layer.conv_relu( stages[-1] , 512 , self.EMBED_SIZE , width=1, padding='SAME', name="512xEmbed" ) )
 
+    stages.append( layer.avg_pool( stages[-1] , stride=4 ) )
+    stages.append( layer.conv_relu( stages[-1] , 512 , self.EMBED_SIZE , width=1, padding='SAME', name="512xEmbed" ) )
     stages.append( tf.nn.sigmoid( tf.reshape(stages[-1], [-1,self.EMBED_SIZE] ), name="image_to_vector") )
+
     stages.append( tf.nn.dropout( stages[-1] , self.keep_prob , name="dropout" )  )
     stages.append( layer.fully_connected( stages[-1], self.EMBED_SIZE, num_classes, name="label_out") )
 
